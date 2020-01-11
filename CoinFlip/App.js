aff30d5517488cbaec4918318cb29828f5513f3d
Celik Berk Altingok <cbaltingok@gmail.com>
2020-01-11 03:17:42 +0300

    Update App.js

--- CoinFlip/App.js
+++ CoinFlip/App.js
@@ -1,17 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
-// import Flipper from './Coin Components/Flipper';
-// import Plate from './Color Box Components/Plate';
-import ButtonList from './ButtonList Comp/ButtonList';
-import NumberList from './NumberList Comp/NumberList';
+import Flipper from './Coin Components/Flipper';
+
 
 function App() {
   return (
     <div >
-      {/* <Flipper/> */}
-      {/* <Plate/> */}
-      {/* <ButtonList/> */}
-      <NumberList/>
+      <Flipper/>
     </div>
   );
 }
