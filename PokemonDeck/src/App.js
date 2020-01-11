d65663a2423ea8289b9178ad56da83a3c0efc89b
Celik Berk Altingok <cbaltingok@gmail.com>
2020-01-11 03:29:05 +0300

    Update App.js

--- PokemonDeck/src/App.js
+++ PokemonDeck/src/App.js
@@ -1,10 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-// import food from './foods';
-// import {choice, remove} from './helpers';
-// import { render } from 'react-dom';
-// import Dog from './Dog';
-// import Pokemon from './Pokemon';
 import Pokegame from './Pokegame';
 
 class App extends Component {
@@ -16,33 +11,5 @@ class App extends Component {
     );
   }
 }
-
-// class App extends Component {
-//   render() {
-//     return(
-//       <div className='App'>
-//         <Dog/>
-//       </div>
-//     );
-//   }
-// }
-
-// class App extends Component {
-
-//   render(){
-//   let fruit = choice(food);
-//   let remaining = remove(food, fruit);
-
-//   return (
-//       <div> 
-//         <p>I would like one {fruit}, please</p>
-//         <p>Here you go: {fruit}</p>
-//         <p>Remaining: {remaining}</p>
-//       </div>
-//     );
-//   }
-// }
-
-
 
 export default App;
